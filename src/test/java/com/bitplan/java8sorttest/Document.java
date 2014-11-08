26654e8cd497d59d8689141632c9c7c5bedea359
Wolfgang Fahl <wf@bitplan.com>
2014-11-08 17:24:52 +0100

    moves types to new file

---
+++ src/test/java/com/bitplan/java8sorttest/Document.java
@@ -0,0 +1,6 @@
+package com.bitplan.java8sorttest;
+
+public interface Document {
+	public String getName();
+	public Folder getParentFolder();
+}
--- src/test/java/com/bitplan/java8sorttest/TestEclipseLinkSorting.java
+++ src/test/java/com/bitplan/java8sorttest/TestEclipseLinkSorting.java
@@ -1,10 +1,5 @@
 package com.bitplan.java8sorttest;
 
-import java.io.Serializable;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -12,23 +7,9 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Persistence;
-import javax.persistence.Table;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import org.junit.Test;
 
@@ -45,153 +26,6 @@ import org.junit.Test;
  */
 public class TestEclipseLinkSorting {
 	protected static Logger LOGGER = Logger.getLogger("com.bitplan.storage.sql");
-
-	public static interface Document {
-		public String getName();
-		public Folder getParentFolder();
-	}
-
-	@Entity(name="Document")
-	@Table(name = "document")
-	public static class DocumentImpl implements Document, Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1206897141744490247L;
-	  
-		@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-	  
-		String name;
-		Folder parentFolder;
-
-		/**
-		 * @return the parentFolder
-		 */
-		@ManyToOne(targetEntity=Folder.class)
-		public Folder getParentFolder() {
-			return parentFolder;
-		}
-
-		/**
-		 * @param parentFolder the parentFolder to set
-		 */
-		public void setParentFolder(Folder parentFolder) {
-			this.parentFolder = parentFolder;
-		}
-
-		/**
-		 * default constructor to make jaxb happy
-		 */
-		public DocumentImpl(){};
-		
-		public DocumentImpl(String pName) {
-			name = pName;
-		}
-
-		public String getName() {
-			return name;
-		}
-		
-		/**
-		 * @param name the name to set
-		 */
-		public void setName(String name) {
-			this.name = name;
-		}
-	}
-
-	@XmlRootElement
-	@Entity(name="Folder")
-	@Table(name = "folder")
-	public static class Folder implements Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -590883790792959372L;
-		
-		List<Document> documents;
-
-		/**
-		 * default constructor for JaxB
-		 */
-		public Folder() {
-		};
-		
-	  @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-		/**
-		 * @return the documents
-		 */
-		@XmlElementWrapper(name = "document")
-		@XmlElement(type=DocumentImpl.class)
-	  @OneToMany(targetEntity=DocumentImpl.class, cascade=CascadeType.ALL, mappedBy="parentFolder")
-		public List<Document> getDocuments() {
-			return documents;
-		}
-
-		/**
-		 * @param documents
-		 *          the documents to set
-		 */
-		public void setDocuments(List<Document> documents) {
-			this.documents = documents;
-		}
-
-		public List<Document> getDocumentsByModificationDate() {
-			List<Document> docs = this.getDocuments();
-			LOGGER.log(Level.INFO, "sorting " + docs.size() + "documents by name");
-			Comparator<Document> comparator = new ByNameComparator();
-			Collections.sort(docs, comparator);
-			return docs;
-		}
-		
-		/**
-		 * marshal me to an xml string
-		 * @return
-		 * @throws Exception
-		 */
-		public String asXML() throws Exception {
-			JAXBContext context = JAXBContext.newInstance(Folder.class);
-			Marshaller marshaller = context.createMarshaller();
-			java.io.StringWriter sw = new StringWriter();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			marshaller.marshal(this, sw);
-			String result=sw.toString();
-			return result;
-		}
-		
-		/**
-		 * get a Folder from an XML String
-		 * @param xml
-		 * @return
-		 * @throws Exception
-		 */
-		public static Folder fromXML(String xml) throws Exception {
-			JAXBContext jaxbContext = JAXBContext.newInstance(Folder.class);
-			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-			StringReader reader = new StringReader(xml);
-			Folder folder = (Folder) unmarshaller.unmarshal(reader);
-			return folder;
-		}
-		
-		/**
-		 * get a folder example
-		 * @return
-		 */
-		public static Folder getFolderExample() {
-			List<Document> documents = new ArrayList<Document>();
-			documents.add(new DocumentImpl("test2"));
-			documents.add(new DocumentImpl("test1"));
-			Folder folder = new Folder();
-			folder.setDocuments(documents);
-			return folder;
-		}
-	}
 
 	public static class ByNameComparator implements Comparator<Document> {
 
